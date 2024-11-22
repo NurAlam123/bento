0ad37452b2c1b20a79d1a05df8dc8c498b99d57b
Nur Alam <nuralam.rsc@gmail.com>
2024-11-23 01:49:26 +0600

    feat: add footer

--- src/components/BentoGrid.tsx
+++ src/components/BentoGrid.tsx
@@ -1,18 +1,24 @@
+import Footer from "./Footer";
 import { Hero, PortfolioLink, Projects, Socials, Welcome } from "./grids";
 import HeadingBox from "./HeadingBox";
+import { Box } from "./ui";
 
 const BentoGrid = () => {
   return (
     <section className="p-6 mt-6 max-w-4xl mx-auto">
       <HeadingBox />
       {/* Bento starts from here */}
-      <div className="grid grid-cols-12 grid-flow-dense gap-4 w-full">
+      <div className="grid grid-cols-12 grid-flow-dense gap-4 w-full mb-8">
         <Hero />
         <Welcome />
         <PortfolioLink />
         <Socials />
         <Projects />
+        <Box className="bg-zinc-100">
+          <p className="text-center font-bold text-4xl">END</p>
+        </Box>
       </div>
+      <Footer />
     </section>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,39 @@
+import { ArrowUpRight } from "@/assets/icons";
+
+const Footer = () => {
+  const createdDate = "2023";
+  const currentDate = new Date().getFullYear().toString();
+  return (
+    <footer className="flex flex-col gap-6 md:gap-0 md:flex-row justify-between text-zinc-700 md:items-center">
+      <div>
+        <h4 className="text-3xl text-zinc-800 font-medium mb-2">Lightento</h4>
+        <p className="text-md">
+          Nur Alam &copy; {createdDate}
+          {currentDate !== createdDate && `- ${currentDate}`}
+        </p>
+      </div>
+      <div className="flex gap-6 *:underline *:underline-offset-2 *:flex *:relative">
+        <a href="https://github.com/nuralam123/bento" target="_blank">
+          Github
+          <span className="absolute -top-2 -right-3">
+            <ArrowUpRight className="w-4" />
+          </span>
+        </a>
+        <a href="https://nuralam.vercel.app" target="_blank">
+          Portfolio{" "}
+          <span className="absolute -top-2 -right-3">
+            <ArrowUpRight className="w-4" />
+          </span>
+        </a>
+        <a href="https://nuralam.vercel.app/#contact" target="_blank">
+          Contact
+          <span className="absolute -top-2 -right-3">
+            <ArrowUpRight className="w-4" />
+          </span>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
