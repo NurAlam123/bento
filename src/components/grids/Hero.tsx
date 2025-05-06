9445e2d640286c0cfa6ab3782e84bb7caa8e639d
Nur Alam <nuralam.rsc@gmail.com>
2025-05-06 22:58:32 +0600

    fix: fix hero section

--- src/components/grids/Hero.tsx
+++ src/components/grids/Hero.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import { SlideUpBox } from "../animaiton/layout-animation";
 import { useState } from "react";
 import MotionDiv from "../ui/MotionDiv";
-import { AnimatePresence, MotionProps, Variants } from "framer-motion";
-import { ArrowDown, ArrowUp } from "lucide-react";
+import { MotionProps, Variants } from "framer-motion";
 
 const firstOneVariant: Variants = {
   initial: {
@@ -27,68 +25,21 @@ const firstOneVariant: Variants = {
   },
 };
 
-const secondOneVariant: Variants = {
-  initial: {
-    y: "100%",
-  },
-  animate: {
-    y: 0,
-    transition: {
-      duration: 0.3,
-      ease: "easeOut",
-    },
-  },
-  exit: {
-    y: "100%",
-    transition: {
-      duration: 0.3,
-      ease: "easeOut",
-    },
-  },
-};
-
 const Hero = () => {
   const [flip, setFlip] = useState(false);
 
   return (
-    <SlideUpBox
-      transition={{
-        delay: 1.1,
-      }}
-      boxVariant="image"
-      onHoverStart={() => setFlip(true)}
-      onHoverEnd={() => setFlip(false)}
-      className="relative overflow-hidden w-fit h-fit"
-    >
-      <p className="absolute font-medium text-3xl top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 -z-10 flex justify-center items-center">
-        {flip ? <ArrowDown /> : <ArrowUp />} &nbsp;Page {flip ? 2 : 1}
-      </p>
-      <a href="https://www.tumblr.com/theycantalk/762363162492289025/idea?source=share">
-        <AnimatePresence mode="wait">
-          {!flip ? (
-            <ImageContainer
-              key="part-1"
-              src="/i_just_got_an_idea.webp"
-              alt="Goat saying I just got a crazy idea - picture is form theycantalk.com"
-              exit="exit"
-              initial="initial"
-              animate="animate"
-              variants={firstOneVariant}
-            />
-          ) : (
-            <ImageContainer
-              key="part-2"
-              src="/i_just_got_an_idea_2.webp"
-              exit="exit"
-              initial="initial"
-              animate="animate"
-              variants={secondOneVariant}
-              alt="Goat saying what if we stop listening the dog - picture is form theycantalk.com"
-            />
-          )}
-        </AnimatePresence>
-      </a>
-    </SlideUpBox>
+    <a href="https://www.tumblr.com/theycantalk/762363162492289025/idea?source=share">
+      <ImageContainer
+        key="part-1"
+        src="/i_just_got_an_idea.webp"
+        alt="Goat saying I just got a crazy idea - picture is form theycantalk.com"
+        exit="exit"
+        initial="initial"
+        animate="animate"
+        variants={firstOneVariant}
+      />
+    </a>
   );
 };
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import projects from "./projects";
 import { socialLinks } from "./socials";
 
-export const spotifyTrackID = "5QO79kh1waicV47BqGRL3g";
+export const spotifyTrackID = "3USxtqRwSYz57Ewm6wWRMp";
 export const pageGithub = socialLinks.github.href + "/bento";
 export { projects };
